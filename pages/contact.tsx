9cefe583862fa23d68d85874ea805b14a27e508a
w.benarto <w.benarto@gmail.com>
2021-11-30 16:15:42 -0800

    deployin

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -28,7 +28,6 @@ const Contact = () => {
             </div>
             <div className={styles.textBox}>
             <p>Jin Choi s a photographer and videographer based in San Francisco, California. With inspiration and creativity at heart, he has a knack for capturing clean, wide open spaces as well as memorable moments.
-He is also a .
 <br />
 <br />Available for hire within photography, video and aerial content. </p>
             </div>

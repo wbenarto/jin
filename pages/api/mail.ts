761cb48f6999e7b507a996aefe364f786a04ecfa
w.benarto <w.benarto@gmail.com>
2021-11-30 16:20:28 -0800

    api error

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-export default (req, res) => {
-  const body = JSON.parse(req.body);
-  console.log(body)
-  res.status(200).json({ status: 'Ok' })
-}
+// export default (req, res) => {
+//   const body = JSON.parse(req.body);
+//   console.log(body)
+//   res.status(200).json({ status: 'Ok' })
+// }

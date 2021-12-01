07f5d8548b1460f6461f28cce45a0c79991a5124
w.benarto <w.benarto@gmail.com>
2021-11-30 16:24:20 -0800

    test

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -4,3 +4,16 @@
 //   console.log(body)
 //   res.status(200).json({ status: 'Ok' })
 // }
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ name: 'John Doe' })
+}

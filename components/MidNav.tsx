8032e9ac882bc3e19199e5ee84c82553fe764e0f
w.benarto <w.benarto@gmail.com>
2022-05-13 15:09:34 -0700

    socials

--- components/MidNav.tsx
+++ components/MidNav.tsx
@@ -1,22 +1,32 @@
 import styles from '../styles/Content.module.css'
+import Link from 'next/link'
+
 
 const MidNav = () => {
     return (
         <div className={styles.midNav}>
             <img className={styles.midBackground} src='/images/sand-1.png' />
             <div className={styles.midLinks}>
-                <div className={styles.midLink}>
-                    <div className={styles.bottomFade} />
-                    <img className={styles.midLinkPic} src='images/jin-dj.JPG' />
-                </div>
-                <div className={styles.midLink}>
-                    <img className={styles.midLinkPic} src='images/jin2.jpg' />
+                {/* <div className={styles.bottomFade} /> */}
 
-                </div>
-                <div className={styles.midLink}>
-                    <img className={styles.midLinkPic} src='images/sunset.jpg' />
-                    link 3
-                </div>
+                <Link href='https://soundcloud.com/djinn415'>
+                    <div className={styles.midLink}>
+                        <img className={styles.midLinkPic} src='images/jin-dj.JPG' />
+                    </div>
+                </Link>
+
+                <Link href='/products'>
+                    <div className={styles.midLink}>
+                        <img className={styles.midLinkPic} src='images/jin2.jpg' />
+                    </div>
+                </Link>
+
+                <Link href='/about'>
+                    <div className={styles.midLink}>
+                        <img className={styles.midLinkPic} src='images/sunset.jpg' />
+                    </div>
+                </Link>
+
             </div>
         </div>
     )
--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,12 +1,19 @@
 import styles from '../styles/Content.module.scss'
 import { FaInstagram, FaFacebook, FaSoundcloud } from 'react-icons/fa'
+import Link from 'next/link'
 
 const Socials = () => {
     return (
         <div className={styles.socialsContainer}>
-            <FaInstagram className={styles.icons} />
-            <FaFacebook className={styles.icons} />
-            <FaSoundcloud className={styles.icons} />
+            <Link href='https://www.facebook.com/TheDjinn'>
+                <FaFacebook className={styles.icons} />
+            </Link>
+            <Link href='https://www.instagram.com/djinn415/'>
+                <FaInstagram className={styles.icons} />
+            </Link>
+            <Link href='https://soundcloud.com/djinn415'>
+                <FaSoundcloud className={styles.icons} />
+            </Link>
 
         </div>
     )

5c8b4de9b0251a4450e3c5d409acfadc7dead1a3
w.benarto <w.benarto@gmail.com>
2022-08-01 18:56:52 -0700

    test cases

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -4,13 +4,29 @@
 
 describe('My First test Suite', function() {
     it('My First Test Case', ()=>{
-        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        // cy.visit('https://jin-six.vercel.app/');
+        // cy.get('.Navbar_navButton__TAATL').click()
+        //fixture
+        cy.get('.search-keyword').type('ca')
+        cy.get('.product:visible').should('have.length', 4)
+        cy.get('.products').find('.product').should('have.length', 4)
+
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+        
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            const textVeg = $el.find('h4.product-name').text()
+            
+            if (textVeg.includes('Cashews')) {
+                cy.wrap($el).find('button').click()
+            }
+        } )
     })
 })
 
 
 describe('My Second test Suite', function() {
     it('My First Test Case', ()=>{
-        
+
     })
 })

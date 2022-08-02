f26b69c80eef2e8274978a1201037ca9feda1052
w.benarto <w.benarto@gmail.com>
2022-08-02 15:21:55 -0700

    pageObject

--- cypress/integration/examples/Test8.js
+++ cypress/integration/examples/Test8.js
@@ -1,4 +1,6 @@
 /// <reference types="Cypress" />
+import HomePage from '../pageObjects/HomePage'
+import ProductPage from '../pageObjects/ProductPage'
 
 describe('Framework Test Angular', function() {
     before(function(){
@@ -12,19 +14,44 @@ describe('Framework Test Angular', function() {
     })
     
     it("Test Case", function(){
+        // Explicitly declare spec files 
+        Cypress.config('defaultCommandTimeout', 8000)
+        const homePage = new HomePage()
+        const productPage = new ProductPage()
+        
         cy.visit('https://rahulshettyacademy.com/angularpractice/')
 
-        cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
-        cy.get('select').select(this.data.gender)
-        cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name)
-        cy.get('input[name="name"]:nth-child(2)').should('have.attr', 'minlength', '2')
-        cy.get('#inlineRadio3').should('be.disabled')
-        cy.get(':nth-child(2) > .nav-link').click()
-        cy.selectProduct('Blackberry')
-        cy.selectProduct('Nokia Edge')
+        homePage.getEditBox().type(this.data.name)
+        homePage.getGender().select(this.data.gender)
+        homePage.getTwoWayDataBinding().should('have.value', this.data.name)
+        homePage.getEditBox().should('have.attr', 'minlength', '2')
+        homePage.getEntrepreneur().should('be.disabled')
+
+        homePage.getShopTab().click()
+
+        // cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
+        // cy.get('select').select(this.data.gender)
+        // cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name)
+        // cy.get('input[name="name"]:nth-child(2)').should('have.attr', 'minlength', '2')
+        // cy.get('#inlineRadio3').should('be.disabled')
+        // cy.get(':nth-child(2) > .nav-link').click()
+        // cy.selectProduct('Blackberry')
+        // cy.selectProduct('Nokia Edge')
 
         this.data.productName.forEach((el) => {
             cy.selectProduct(el)
+        })
+
+        productPage.checkOutButton().click()
+        cy.contains('Checkout').click()
+        cy.get('#country').type('India')
+        cy.get('.suggestions > ul > li > a').click()
+        cy.get('#checkbox2').click({force:true})
+        cy.get('input[type="submit"]').click()
+        // cy.get('.alert').should('have.text', "Success! Thank You! Your order will be deliverered in next few weeks :-)")
+        cy.get('.alert').then(function(el){
+            const text = el.text()
+            expect(text.includes('Success')).to.be.true
         })
     })
 })
